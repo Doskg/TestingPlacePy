481e826878d49d29c11478a894a08373a72e998f
Oscar González Baldizon <oscar.gonzalez.baldizon@gmail.com>
2024-05-01 18:34:42 -0600

    e

--- youtube/1.py
+++ youtube/1.py
@@ -3,4 +3,7 @@ Vamos a practicar con ejercicios de Youtube
 
 """
 
-print("cual es tu nombre?? ")
+name=input("cual es tu nombre?? ")
+a=int(input("cual es tu edad?? "))
+
+print("hola",name,"tu edad es",a)
